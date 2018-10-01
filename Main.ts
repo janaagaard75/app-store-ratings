afac0c3e14717faa68ffde1ba0b57bec4651bc3f
Jan Aagaard <jan@aagaard.net>
2018-10-01 22:03:23 +0200

    Basic fetch

--- Main.ts
+++ Main.ts
@@ -1,6 +1,16 @@
+import fetch from 'node-fetch'
+
 class Main {
-  public start() {
-    console.log('Hello world.')
+  public async start() {
+    const reviewsResponse = await fetch('https://itunes.apple.com/dk/rss/customerreviews/id=571242024/sortBy=mostRecent/page=10/limit=200/json')
+
+    if (reviewsResponse.status !== 200) {
+      console.error(`Error. Returned code: ${reviewsResponse.status}.`)
+      return
+    }
+
+    const content = await reviewsResponse.text()
+    console.log(content)
   }
 }
 
