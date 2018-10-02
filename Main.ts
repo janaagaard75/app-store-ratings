c370552d9ac7331231714f404e590e72d1651675
Jan Aagaard <jan@aagaard.net>
2018-10-02 21:18:22 +0200

    Flattern the first three properties

--- Main.ts
+++ Main.ts
@@ -1,8 +1,22 @@
 import fetch from 'node-fetch'
 
+import { Entry } from './Reviews'
+import { ReleaseDate } from './Reviews'
 import { Reviews } from './Reviews'
 
+interface FlatReview {
+  author: string
+  content: string
+  date: Date | undefined
+  rating: number
+  title: string
+  version: string
+  voteCount: number
+  voteSum: number
+}
+
 type Page = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+
 type SortBy = 'mostHelpful' | 'mostRecent'
 
 class Main {
@@ -25,6 +39,32 @@ class Main {
 
     const reviews = await reviewsResponse.json() as Reviews
     return reviews
+  }
+
+  private flattern(reviews: Reviews): ReadonlyArray<FlatReview> {
+    const mapped = reviews.feed.entry
+      .map(entry => {
+        return {
+          author: entry.author ? entry.author.name.label : '',
+          content: entry.content ? entry.content.label : '',
+          date: Main.getDate(entry),
+          rating: 0,
+          title: '',
+          version: '',
+          voteCount: 0,
+          voteSum: 0
+        }
+      })
+
+    return mapped
+  }
+
+  private static getDate(entry: Entry): Date | undefined {
+    if (entry['im:releaseDate'] === undefined) {
+      return undefined
+    }
+
+    return new Date((entry['im:releaseDate'] as ReleaseDate).label)
   }
 }
 
