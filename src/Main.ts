3f9bc7e3729d1e419f8eeb030db7e4bf0c62975c
Jan Aagaard <jan@aagaard.net>
2018-10-03 23:02:09 +0200

    Loop through pages

--- src/Main.ts
+++ src/Main.ts
@@ -5,24 +5,33 @@ import { Entry } from "./Reviews"
 import { EntryWrapper } from "./EntryWrapper"
 import { Reviews } from "./Reviews"
 
-type Page = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
-
 type SortBy = "mostHelpful" | "mostRecent"
 
 class Main {
   public async start() {
     const nordeaMobileBankAppId = 571242024
-    const xmlReviews = await this.fetchReviews(nordeaMobileBankAppId, "mostRecent", 1)
-    const parsedReviews = this.flattern(xmlReviews.feed.entry)
-    console.log(`Reviews: ${xmlReviews.feed.entry.length}, ${parsedReviews.length}`)
-    const csv = parsedReviews.map(review => review.csvLine).join("\n")
+
+    const allReviews: Array<EntryWrapper> = []
+    for (let page = 1; page <= 10; page++) {
+      // Skip page number 6 because the XML parser complains about an unsupported character (&) on that page.
+      if (page === 6) {
+        continue
+      }
+
+      const xmlReviews = await this.fetchReviews(nordeaMobileBankAppId, "mostRecent", page)
+      const parsedReviews = this.flattern(xmlReviews.feed.entry)
+      allReviews.push(...parsedReviews)
+    }
+    console.log(`Number of reviews: ${allReviews.length}.`)
+    const csv = allReviews.map(review => review.csvLine).join("\n")
     console.info(`\n${csv}`)
   }
 
   private async fetchReviews(
     appId: number,
     sortBy: SortBy,
-    pageNumber: Page
+    /** Integer from 1 to 10. */
+    pageNumber: number
   ): Promise<Reviews> {
     const reviewsUrl = `https://itunes.apple.com/dk/rss/customerreviews/id=${appId}/sortBy=${sortBy}/page=${pageNumber}/xml`
     const reviewsResponse = await fetch(reviewsUrl)
